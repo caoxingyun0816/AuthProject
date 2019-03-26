ca431d3adf0e48cc2c47ba08ab9eefc892f80fd0
caoxingyun <cxydy0816>
2019-03-26 17:26:31 +0800

    自定义用户服务

---
+++ cxy-security-browser/src/main/java/com/cxy/security/cxysecuritybrowser/config/BrowserSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cxy.security.cxysecuritybrowser.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+/***
+ * Created by Caoxingyun on 2019/03/26
+ */
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.formLogin()//表单登录
+                //http.httpBasic() 默认登录
+        .and().authorizeRequests().anyRequest().authenticated();//任何请求都需要验证
+    }
+}
